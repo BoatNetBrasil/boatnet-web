c54ea3e874655fd867443508a59d086971263ca4
BoatNetBrasil <contato@boatnet.com.br>
2026-02-20 05:00:46 -0300

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,11 @@
 export {}
 
 declare global {
+  type Preset = 'default' | 'partner' | 'booking' | 'support'
+
   interface Window {
     BNChat?: {
-      open: (args?: { preset?: string }) => void
+      open: (args?: { preset?: Preset }) => void
       close: () => void
       reset: () => void
     }
